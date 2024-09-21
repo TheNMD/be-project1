e57d122d97c1bc5cb55eaf64942a505a10f4b671
DanNguyen <manhdanog@gmail.com>
2024-09-21 11:39:30 +0700

    update

--- exercises/lesson_13/src/main/java/be_project1/lesson_13/controller/Init.java
+++ exercises/lesson_13/src/main/java/be_project1/lesson_13/controller/Init.java
@@ -1,7 +1,11 @@
 package be_project1.lesson_13.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import be_project1.lesson_13.dto.request.User;
 
 @RestController
 public class Init {
@@ -11,8 +15,10 @@ public class Init {
         return "Hello course online project";
     }
 
-    @GetMapping("/testing")
-    public String Welcome1() {
-        return "Testing another API";
+    @PostMapping("/testing")
+    public User createUser(@RequestBody User user) {
+        // TODO Hash passwords
+        user.setPassword("NotARealPassword"); 
+        return user;
     }
 }
