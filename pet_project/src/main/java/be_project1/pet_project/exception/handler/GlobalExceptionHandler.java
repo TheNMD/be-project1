700c734fc8283cfc4d1c301a94af8f08767fb35a
DanNguyen <manhdanog@gmail.com>
2024-10-20 18:00:47 +0700

    update

--- pet_project/src/main/java/be_project1/pet_project/exception/handler/GlobalExceptionHandler.java
+++ pet_project/src/main/java/be_project1/pet_project/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.validation.BindException;
 
 import java.util.HashMap;
@@ -12,13 +13,14 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    // Bind Exception
     @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<?> handleBindException(BindException e) {
         Map<String, String> errors = new HashMap<>();
         e.getBindingResult()
-         .getFieldErrors()
-         .forEach(error ->errors.put(error.getField(), error.getDefaultMessage()));
+        .getFieldErrors()
+        .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
     }
 }
